1164ec4e58d1f31f4b05040a68f883955c857e20
Anton K. <antonkarlovskiy@outlook.com>
2025-12-04 00:37:54 -0500

    feat: add Tabs component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import { Button } from "@/components/ui/button";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Button>Click me</Button>
-    </div>
-  );
+    <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList>
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="password">Password</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">Make changes to your account here.</TabsContent>
+      <TabsContent value="password">Change your password here.</TabsContent>
+    </Tabs> 
+  )
 }
